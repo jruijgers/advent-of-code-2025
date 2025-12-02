4e7cba4d1451337975e94920e8e4ca09772939c6
Jeroen J. Ruijgers <j.ruijgers@belastingdienst.nl>
2025-12-02 14:25:28 +0100

    Day 2 gebruik parallelStream, 1x parsen input

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -1,67 +1,44 @@
 void main() throws URISyntaxException, IOException {
 	String input = Files.readAllLines(Path.of(ClassLoader.getSystemResource("02.input").toURI())).getFirst();
+	List<Long> numbers = Arrays.stream(input.split(",")).flatMap(s -> toRange(s)).toList();
 	System.out.print("2.1: ");
-	Arrays.stream(input.split(",")).flatMap(s -> part1(s)).reduce(Long::sum).ifPresent(System.out::println);
+	numbers.parallelStream().filter(s -> part1(s)).reduce(Long::sum).ifPresent(System.out::println);
 	System.out.print("2.2: ");
-	Arrays.stream(input.split(",")).flatMap(s -> part2(s)).reduce(Long::sum).ifPresent(System.out::println);
+	numbers.parallelStream().filter(s -> part2(s)).reduce(Long::sum).ifPresent(System.out::println);
 }
 
-private static Stream<Long> part1(String s) {
+private Stream<Long> toRange(String s) {
 	String[] numbers = s.split("-");
 
-	long start = Long.parseLong(numbers[0]);
-	long end = Long.parseLong(numbers[1]);
-
-	List<Long> list = new ArrayList<>();
-
-	for (long i = start; i <= end; i++) {
-		String current = Long.toString(i);
-
-		String s1 = current.substring(0, current.length() / 2);
-		String s2 = current.substring(current.length() / 2, current.length());
-
-		if (s1.equals(s2)) {
-			list.add(i);
-		}
-	}
-
-	return list.stream();
+	return LongStream.rangeClosed(Long.parseLong(numbers[0]), Long.parseLong(numbers[1])).boxed();
 }
 
-private static Stream<Long> part2(String s) {
-	String[] numbers = s.split("-");
+private boolean part1(Long number) {
+	String current = Long.toString(number);
 
-	long start = Long.parseLong(numbers[0]);
-	long end = Long.parseLong(numbers[1]);
+	return current.substring(0, current.length() / 2).equals(current.substring(current.length() / 2));
+}
 
-	List<Long> list = new ArrayList<>();
+private boolean part2(Long number) {
+	String current = Long.toString(number);
 
-	for (long number = start; number <= end; number++) {
-		String current = Long.toString(number);
+	for (int patternLength = 1; patternLength <= current.length() / 2; patternLength++) {
+		if (current.length() % patternLength == 0) {
+			String pattern = current.substring(0, patternLength);
 
-		boolean added = false;
-		for (int patternLength = 1; !added && patternLength <= current.length() / 2; patternLength++) {
-			if (current.length() % patternLength == 0) {
-				String pattern = current.substring(0, patternLength);
+			boolean completePattern = true;
 
-				boolean completePattern = true;
+			for (int b = patternLength; completePattern && b + patternLength <= current.length(); b += patternLength) {
+				String s2 = current.substring(b, b + patternLength);
 
-				int b = patternLength;
-				while (completePattern && b + patternLength <= current.length()) {
-					String s2 = current.substring(b, b + patternLength);
+				completePattern = completePattern && s2.equals(pattern);
+			}
 
-					completePattern = completePattern && s2.equals(pattern);
-
-					b += patternLength;
-				}
-
-				if (completePattern) {
-					list.add(number);
-					added = true;
-				}
+			if (completePattern) {
+				return true;
 			}
 		}
 	}
 
-	return list.stream();
+	return false;
 }
