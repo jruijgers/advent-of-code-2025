39282a478bec6538d05fc770a0949b0b1322a9dd
Jeroen J. Ruijgers <j.ruijgers@belastingdienst.nl>
2025-12-03 11:57:46 +0100

    some optimazations

--- src/main/java/Day03.java
+++ src/main/java/Day03.java
@@ -5,27 +5,30 @@ import java.nio.file.Path;
 import java.util.List;
 
 void main() throws URISyntaxException, IOException {
-	List<String> strings = Files.readAllLines(Path.of(ClassLoader.getSystemResource("03.input").toURI()));
+	List<short[]> strings = Files.lines(Path.of(ClassLoader.getSystemResource("03.input").toURI())).map(this::toValues).toList();
 
-	part1(strings, 2);
-	part1(strings, 12);
+	System.out.print("3.1: ");
+	strings.parallelStream().map(s -> toJoltage(s, 2)).reduce(Long::sum).ifPresent(System.out::println);
+	System.out.print("3.2: ");
+	strings.parallelStream().map(s -> toJoltage(s, 12)).reduce(Long::sum).ifPresent(System.out::println);
 }
 
-private void part1(List<String> strings, int cellsToTurnOn) {
-	strings.parallelStream().map(s -> toJoltage(s, cellsToTurnOn)).reduce(Long::sum).ifPresent(System.out::println);
-}
-
-private long toJoltage(String input, int cellsToTurnOn) {
-	var values = new long[input.length()];
-	var cells = new long[cellsToTurnOn];
+private short[] toValues(String input) {
+	var values = new short[input.length()];
 
 	for (int i = 0; i < input.length(); i++) {
-		values[i] = Long.parseLong(input.substring(i, i + 1));
+		values[i] = Short.parseShort(input.substring(i, i + 1));
 	}
+
+	return values;
+}
+
+private long toJoltage(short[] values, int cellsToTurnOn) {
+	var cells = new short[cellsToTurnOn];
 
 	int lastCellPos = -1;
 	for (int cell = 0; cell < cellsToTurnOn; cell++) {
-		for (int i = lastCellPos + 1; i < input.length() - cellsToTurnOn + cell + 1; i++) {
+		for (int i = lastCellPos + 1; i < values.length - cellsToTurnOn + cell + 1; i++) {
 			if (values[i] > cells[cell]) {
 				cells[cell] = values[i];
 				lastCellPos = i;
@@ -34,7 +37,7 @@ private long toJoltage(String input, int cellsToTurnOn) {
 	}
 
 	long sum = 0;
-	for (int i = 0; i < cells.length; i++) {
+	for (int i = 0; i < cellsToTurnOn; i++) {
 		sum += cells[i] * Math.powExact(10L, cellsToTurnOn - i - 1);
 	}
 	return sum;
