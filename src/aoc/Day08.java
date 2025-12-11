96d5d32735e56bd6a06cc5c97ecbec6ef06e88ae
Jeroen J. Ruijgers <j.ruijgers@belastingdienst.nl>
2025-12-11 13:50:03 +0100

    day 8 in 1 go

--- src/aoc/Day08.java
+++ src/aoc/Day08.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -17,43 +15,20 @@ import java.util.stream.Collectors;
 
 public class Day08 {
 	public void main() throws URISyntaxException, IOException {
-		List<LightSocket> lightSockets = Files.lines(Path.of(ClassLoader.getSystemResource("08.input").toURI())).map(LightSocket::new)
-				.toList();
-
-		part1(lightSockets, 1000);
-		part2(lightSockets);
-	}
-
-	private void part1(List<LightSocket> lightSockets, int limit) {
+		List<LightSocket> lightSockets = Files.lines(Path.of(ClassLoader.getSystemResource("08.input").toURI()))
+				.map(LightSocket::new).toList();
 
 		Map<LightSocket, Set<LightSocket>> chains = lightSockets.stream()
 				.collect(Collectors.toMap(ls -> ls, ls -> new HashSet<>(singletonList(ls))));
 
-		lightSockets.stream().flatMap(a -> lightSockets.stream().filter(b -> a != b).map(b -> a.distance(b)).distinct()).distinct()
-				.sorted().limit(limit).forEach(distance -> {
-					Set<LightSocket> chainLeft = chains.get(distance.left);
-					Set<LightSocket> chainRight = chains.get(distance.right);
-
-					if (chainLeft != chainRight) {
-						chainLeft.addAll(chainRight);
-						for (LightSocket lightSocket : chainRight) {
-							chains.replace(lightSocket, chainLeft);
-						}
-					}
-				});
-
-		chains.values().stream().distinct().map(Set::size).sorted(Collections.reverseOrder()).limit(3).reduce(Math::multiplyExact)
-				.ifPresent(s -> System.out.println("8.1: " + s));
-	}
-
-	private void part2(List<LightSocket> lightSockets) {
-		Map<LightSocket, Set<LightSocket>> chains = lightSockets.stream()
-				.collect(Collectors.toMap(ls -> ls, ls -> new HashSet<>(singletonList(ls))));
-
+		int i = 0;
 		List<LightSocket.Distance> distances = lightSockets.stream()
-				.flatMap(a -> lightSockets.stream().filter(b -> a != b).map(b -> a.distance(b)).distinct()).distinct().sorted().toList();
+				.flatMap(a -> lightSockets.stream().filter(b -> a != b).map(b -> a.distance(b)).distinct()).distinct().sorted()
+				.collect(Collectors.toList());
 
-		for (LightSocket.Distance distance : distances) {
+		LightSocket.Distance distance = null;
+		while (chains.values().stream().distinct().count() > 1) {
+			distance = distances.removeFirst();
 			Set<LightSocket> chainLeft = chains.get(distance.left);
 			Set<LightSocket> chainRight = chains.get(distance.right);
 
@@ -64,14 +39,12 @@ public class Day08 {
 				}
 			}
 
-			if (chains.values().stream().distinct().count() == 1) {
-				System.out.println("8.2: " + (distance.left.x * distance.right.x));
-				break;
+			if (++i == 1000) {
+				chains.values().stream().distinct().map(Set::size).sorted(Collections.reverseOrder()).limit(3)
+						.reduce(Math::multiplyExact).ifPresent(s -> System.out.println("8.1: " + s));
 			}
 		}
 
-//		System.out.println(chains);
-		//		chains.values().stream().distinct().map(Set::size).sorted(Collections.reverseOrder()).limit(3).reduce(Math::multiplyExact)
-		//				.ifPresent(s -> System.out.println("8.1: " + s));
+		System.out.println("8.2: " + (distance.left.x * distance.right.x));
 	}
 }
